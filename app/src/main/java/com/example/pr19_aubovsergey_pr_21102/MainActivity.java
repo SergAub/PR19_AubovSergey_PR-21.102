bed3f78a6c8a946759b8f7d04520827214d27f9a
Sergey <sergeypanz355@mail.ru>
2023-10-04 21:18:27 +0700

    Готово

--- app/src/main/java/com/example/pr19_aubovsergey_pr_21102/MainActivity.java
+++ app/src/main/java/com/example/pr19_aubovsergey_pr_21102/MainActivity.java
@@ -3,12 +3,75 @@ package com.example.pr19_aubovsergey_pr_21102;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.widget.Button;
+import android.widget.TextView;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView timePick;
+    Button btnTime, btnDate;
+    Calendar dateTime = Calendar.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnTime = findViewById(R.id.btnTime);
+        btnDate = findViewById(R.id.btnDate);
+        timePick = findViewById(R.id.timePick);
+
+        btnTime.setOnClickListener(v ->{getTime();});
+        btnDate.setOnClickListener(v -> {getDate();});
+
+        setInitialDateTime();
     }
+
+    private void setInitialDateTime(){
+        timePick.setText(DateUtils.formatDateTime( this,
+            dateTime.getTimeInMillis(),
+                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR
+                    | DateUtils.FORMAT_SHOW_TIME));
+    }
+
+    public void getDate(){
+        new DatePickerDialog(MainActivity.this, d,
+                dateTime.get(Calendar.YEAR),
+                dateTime.get(Calendar.MONTH),
+                dateTime.get(Calendar.DAY_OF_MONTH))
+                .show();
+    }
+    public void getTime(){
+        new TimePickerDialog(MainActivity.this, t,
+                dateTime.get(Calendar.HOUR_OF_DAY),
+                dateTime.get(Calendar.MINUTE), true)
+                .show();
+    }
+
+    TimePickerDialog.OnTimeSetListener t = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            dateTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            dateTime.set(Calendar.MINUTE, minute);
+            setInitialDateTime();
+        }
+    };
+
+    DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            dateTime.set(Calendar.YEAR, year);
+            dateTime.set(Calendar.MONTH, monthOfYear);
+            dateTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            setInitialDateTime();
+        }
+    };
+
 }
